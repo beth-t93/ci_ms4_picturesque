4f7911d3089392bbdf0dc20fca93fe479128931b
Beth Travis <beth_t93@live.co.uk>
2022-04-04 21:51:28 +0100

    fix bug with description not showing on product

--- products/models.py
+++ products/models.py
@@ -14,7 +14,7 @@ class Product(models.Model):
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
-    description = models.TextField
+    description = models.TextField(null=True, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image = models.ImageField(null=True, blank=True)
     in_stock = models.BooleanField(default=True)
