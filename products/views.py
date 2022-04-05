f146101b1ab8a29cdd0fd1a2e85221e12f714dcc
Beth Travis <beth_t93@live.co.uk>
2022-04-05 14:47:56 +0100

    attempt to fix error with products not showing

--- products/views.py
+++ products/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from .models import Product
 
+
 def all_products(request):
+    """A View to display all items"""
+
     products = Product.objects.all()
+
     context = {
         'products': products,
     }
-    return render(request, "products/products.html", context)
-""" A view to show all products """
+
+    return render(request, 'products/products.html', context)
