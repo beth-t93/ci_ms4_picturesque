055fd5c179ba9fc8457a889de89ce2d70a0d1670
Beth Travis <beth_t93@live.co.uk>
2022-04-06 21:56:20 +0100

    remove uneeded notes

--- checkout/models.py
+++ checkout/models.py
@@ -24,15 +24,9 @@ class Order(models.Model):
     grand_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
 
     def _generate_order_number(self):
-        """
-        Generate random order number using
-        """
         return uuid.uuid4().hex.upper()
 
     def update_total(self):
-        """
-        Update grand total
-        """
         self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum']
         if self.order_total < settings.FREE_DELIVERY_THRESHOLD:
             self.delivery_cost = self.order_total * settings.STANDARD_DELIVERY_PERCENTAGE / 100
@@ -42,10 +36,6 @@ class Order(models.Model):
         self.save()
 
     def save(self, *args, **kwargs):
-        """
-        Override the original save method to set the order number
-        if it hasn't been set already.
-        """
         if not self.order_number:
             self.order_number = self._generate_order_number()
         super().save(*args, **kwargs)
