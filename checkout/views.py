5fcf4525c37d059b49cf9eefcf9f3fd75e7c39ac
Beth Travis <beth_t93@live.co.uk>
2022-04-07 00:31:46 +0100

    remove stripe key from views.py

--- checkout/views.py
+++ checkout/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
+from django.conf import settings
+
 from .forms import OrderForm
+from bag.contexts import bag_contents
+
+import stripe
 
 def checkout(request):
     bag = request.session.get('bag', {})
@@ -8,12 +13,14 @@ def checkout(request):
         messages.error(request, "There is nothing here at the moment")
         return redirect(reverse('products'))
     
+    current_bag = bag_contents(request)
+    total = current_bag['grand_total']
+    stripe_total = round(total * 100)
+    
     order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51KOTyFE1zig55cHiLe5KSTyyfmMhHslnmXeEWaoW6GtK6WNyfN7nR7LAtELiuTuFNaVZziL5lExPPQRGkuzd5tDS00sx0vUDDm',
-        'client_secret': 'test client secret',
     }
     
     return render(request, template, context)
