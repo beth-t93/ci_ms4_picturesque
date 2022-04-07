9cba7bd3075500fbb7b978395f0e4258fa9f7ae2
Beth Travis <beth_t93@live.co.uk>
2022-04-07 20:57:16 +0100

    implemented confirmation emails to users

--- picturesque/settings.py
+++ picturesque/settings.py
@@ -167,3 +167,5 @@ STRIPE_CURRENCY = 'gbp'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY')
 
+DEFAULT_FROM_EMAIL = 'picturesque@example.com'
+
