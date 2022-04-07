6fb8fb91a43be39a9afd2429b1d5460b1fde6c6a
Beth Travis <beth_t93@live.co.uk>
2022-04-07 11:32:47 +0100

    add stripe to payments

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,4 @@
+from locale import currency
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
 from django.conf import settings
@@ -8,6 +9,9 @@ from bag.contexts import bag_contents
 import stripe
 
 def checkout(request):
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+    
     bag = request.session.get('bag', {})
     if not bag:
         messages.error(request, "There is nothing here at the moment")
@@ -16,11 +20,23 @@ def checkout(request):
     current_bag = bag_contents(request)
     total = current_bag['grand_total']
     stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+         amount=stripe_total,
+         currency=settings.STRIPE_CURRENCY,
+     )
     
     order_form = OrderForm()
+    
+    if not stripe_public_key:
+        messages.warning(request, 'Stripe public key is missing, did you forget to set it?')
+
+    
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': stripe_public_key, 
+        'client_secret': intent.client_secret,
     }
     
     return render(request, template, context)
