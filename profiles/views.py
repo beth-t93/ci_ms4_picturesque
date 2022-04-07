c00c91e20785be8a2ae20646123e495fbe5e72dc
Beth Travis <beth_t93@live.co.uk>
2022-04-07 17:22:59 +0100

    create order history section for profile

--- profiles/views.py
+++ profiles/views.py
@@ -1,8 +1,11 @@
+from multiprocessing import context
+from re import template
 from django.shortcuts import render, get_object_or_404
 from django.contrib import messages
 
 from .models import UserProfile
 from .forms import UserProfileForm
+from checkout.models import Order
 
 def profile(request):
     """Display user profile"""
@@ -21,6 +24,21 @@ def profile(request):
         'form': form,
         'orders': orders,
         'on_profile_page': True
+    }
+    
+    return render(request, template, context)
+
+def order_history(request, order_number):
+    order = get_object_or_404(Order, order_number=order_number)
+    messages.info(request, (
+        f'This is a confirmation for order number {order_number}.'
+        'A confirmation email was sent on the order date.'
+    ))
+    
+    template = 'checkout/checkout_success.html'
+    context = {
+        'order': order,
+        'from_profile': True,
     }
     
     return render(request, template, context)
