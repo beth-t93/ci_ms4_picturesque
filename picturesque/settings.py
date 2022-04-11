be621aa8371435db8e4cf13bf073d3a312c55e71
Beth Travis <beth_t93@live.co.uk>
2022-04-11 20:10:11 +0100

    add stripe wh secret

--- picturesque/settings.py
+++ picturesque/settings.py
@@ -200,5 +200,6 @@ STANDARD_DELIVERY_PERCENTAGE = 20
 STRIPE_CURRENCY = 'gbp'
 STRIPE_PUBLIC_KEY = os.environ.get('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY', '')
+STRIPE_WH_SECRET = os.environ.get('STRIPE_WH_SECRET', '')
 
 DEFAULT_FROM_EMAIL = 'picturesque@example.com'
