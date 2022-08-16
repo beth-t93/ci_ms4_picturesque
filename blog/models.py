00e3aee71a6aaa979ec687737efd3918cc99ded1
Beth Travis <beth_t93@live.co.uk>
2022-08-16 13:35:20 +0100

    extra widget

--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,10 @@
+from email.policy import default
 from django.db import models
 
 class Post(models.Model):
     title = models.CharField(max_length=255)
     slug = models.SlugField()
-    image = models.ImageField(null=True, blank=True)
+    image = models.ImageField(default='', blank=True)
     body = models.TextField()
     date_added = models.DateTimeField(auto_now_add=True)
 
