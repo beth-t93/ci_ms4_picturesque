467a865d2d3bd3e175e1120abc0d15ca0673a353
Beth Travis <beth_t93@live.co.uk>
2022-08-16 13:39:07 +0100

    update models

--- blog/models.py
+++ blog/models.py
@@ -4,9 +4,13 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=255)
     slug = models.SlugField()
-    image = models.ImageField(default='', blank=True)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
     body = models.TextField()
     date_added = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.name
 
     class Meta:
         ordering = ['-date_added']
