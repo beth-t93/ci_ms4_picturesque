2079de8ae89763358168ad33728b2956f5db9356
Beth Travis <beth_t93@live.co.uk>
2022-08-17 10:35:08 +0100

    update settings.py

--- picturesque/settings.py
+++ picturesque/settings.py
@@ -182,7 +182,7 @@ if 'USE_AWS' in os.environ:
     AWS_STORAGE_BUCKET_NAME = 'picturesque-ms4-2'
     AWS_S3_REGION_NAME = 'eu-west-2'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-    AWS_SECRET_KEY_ID = os.environ.get('AWS_SECRET_KEY_ID')
+    AWS_SECRET_ACCESS_KEY= os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
     
     STATICFILES_STORAGE = 'custom_storages.StaticStorage'
