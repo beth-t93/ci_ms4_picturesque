25520b0ec2b474070966e2571b87edc102ef7af9
Beth Travis <beth_t93@live.co.uk>
2022-08-21 03:51:22 +0100

    change url

--- picturesque/urls.py
+++ picturesque/urls.py
@@ -12,5 +12,5 @@ urlpatterns = [
     path('checkout/', include('checkout.urls')),
     path('profile/', include('profiles.urls')),
     path('contact/', include('contact.urls')),
-    path('blogpage/', include('blog.urls')),
+    path('blog/', include('blog.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
